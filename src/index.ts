13d17dfeefb3a0aa0c0d333e06f099a4a3b56844
firezym <firezym@163.com>
2023-12-25 10:41:37 +0800

    split notification to independent file
    split notification to independent file

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ import {
   runBelowIconNoteChat,
   runSelectedIconNoteChat 
 } from './icon'
+import { showCustomNotification } from './notification'
 
 /**
  * Initialization data for the jupyterlab-notechat extension.
@@ -726,55 +727,6 @@ const processCellSourceString = (
 
   // 返回处理后的字符串
   return lines.join('\n').trim();
-}
-
-
-// 自定义弹出通知界面，在toolbar的下方弹出
-const showCustomNotification = async (
-  message: string,
-  panel: NotebookPanel,
-  timeout: number = 2000
-): Promise<void> => {
-  // 假设 `panel` 是当前的 NotebookPanel 实例
-  const toolbar = panel.toolbar.node
-  const toolbarRect = toolbar.getBoundingClientRect()
-
-  const notification = document.createElement('div')
-  notification.className = 'notification'
-  notification.textContent = message
-  // 设置在工具栏底部
-  notification.style.top = `${toolbarRect.bottom}px`
-
-  document.body.appendChild(notification)
-
-  let timeoutId: number | null = null;
-
-  const removeNotification = () => {
-    if (document.body.contains(notification)) {
-      document.body.removeChild(notification)
-    }
-  }
-
-  // 重新启动倒计时
-  const restartTimeout = () => {
-    if (timeoutId !== null) {
-      clearTimeout(timeoutId)
-    }
-    timeoutId = window.setTimeout(removeNotification, timeout);
-  }
-
-  // 鼠标悬停时清除倒计时
-  notification.addEventListener('mouseenter', () => {
-    if (timeoutId !== null) {
-      clearTimeout(timeoutId)
-    }
-  })
-
-  // 鼠标离开时重新开始倒计时
-  notification.addEventListener('mouseleave', restartTimeout)
-
-  // 开始初始倒计时
-  restartTimeout()
 }
 
 // 按照用户指定的cell id范围，运行之间所有的cell，自动识别需要AI Assistant的回复
