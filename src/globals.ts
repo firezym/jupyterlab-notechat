9375895c7d93f7b8cd8c863c1168b591fe409485
firezym <firezym@163.com>
2023-12-29 15:09:11 +0800

    add user model setting param
    add user model setting param

--- src/globals.ts
+++ src/globals.ts
@@ -15,7 +15,7 @@ export const SETTINGS = {
 // 该参数主要和AI模型有关
 export const CHAT_PARAMS = {
     'prompt': "You are a helpful assistant, especially good at coding and quantitative analysis. You have a good background knowledge in AI, technology, finance, economics, statistics and related fields. Now you are helping the user under a JupyterLab notebook coding environment (format: *.ipynb). You will receive the source codes and outputs of the currently active cell and several preceding cells as your context. Please try to answer the user's questions or solve problems presented in the active cell. Please use simplified Chinese as your primary language to respond :) Switch to English at anytime when it's necessary, or more helpful for understanding and analysis, or instructed to do so.",
-    'model': 'gpt-3.5-turbo',
+    'model': 'gpt-4-1106-preview',
     'vision_model': 'gpt-4-vision-preview',
     'response_format': 'text',
     'temperature': 0.5,
